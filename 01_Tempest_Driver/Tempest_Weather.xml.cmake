adc31ec538570afc72d7d90cadfa12ec71f6d7d4
Kevin Gilliam <keving@arizona.edu>
2022-11-15 08:49:34 -0700

    first commit

---
+++ 01_Tempest_Driver/Tempest_Weather.xml.cmake
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+   <devGroup group="Telescopes">
+      <device label="LFAST Mount Driver" manufacturer="Steward Observatory ETS">
+         <driver name="LFAST Mount Driver">lfast_mount_driver</driver>
+         <version>@CDRIVER_VERSION_MAJOR@.@CDRIVER_VERSION_MINOR@</version>
+      </device>
+   </devGroup>
+</driversList>
+
